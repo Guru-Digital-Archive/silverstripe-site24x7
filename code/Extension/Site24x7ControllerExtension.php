9a838d54dedfe5fadcf6bd869d44aedeb7d55d16
Corey Sewell <corey@gurudigital.nz>
2016-11-21 14:12:26 +1300

    formatting

--- code/Extension/Site24x7ControllerExtension.php
+++ code/Extension/Site24x7ControllerExtension.php
@@ -7,20 +7,19 @@ class Site24x7ControllerExtension extends Extension
 {
     public function onBeforeInit()
     {
+        $isInstalled = false;
         $isCMS       = (bool) is_subclass_of($this->owner, "LeftAndMain");
         $isAjax      = Director::is_ajax();
         $configTable = SiteConfig::current_site_config()->baseTable();
         $dbSchema    = DB::get_schema();
 
-        $isInstalled = false;
         if ($dbSchema->hasTable($configTable)) {
             $configFileds = $dbSchema->fieldList($configTable);
             $isInstalled  = isset($configFileds["RUMKey"]);
         }
 
         if ($isInstalled && !$isCMS && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
-            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"),
-                                                                  [
+            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"), [
                 "RUMKey" => SiteConfig::current_site_config()->RUMKey,
             ]);
         }
