7717824d5b40d6e7d5847010e339a8765ca699d5
Corey Sewell <corey@gurudigital.nz>
2016-11-21 15:28:53 +1300

    Dont do anything if dev build is being called

--- code/Extension/Site24x7ControllerExtension.php
+++ code/Extension/Site24x7ControllerExtension.php
@@ -7,21 +7,23 @@ class Site24x7ControllerExtension extends Extension
 {
     public function onBeforeInit()
     {
-        $isInstalled = false;
-        $isCMS       = (bool) is_subclass_of($this->owner, "LeftAndMain");
-        $isDevBuild  = (bool) is_subclass_of($this->owner, "DevBuildController");
-        $isAjax      = Director::is_ajax();
-        $configTable = "SiteConfig";
-        $dbSchema    = DB::get_schema();
+        $isInstalled         = false;
+        $isIgnoredController = (bool) $this->owner instanceof LeftAndMain ||
+            $this->owner instanceof DevelopmentAdmin ||
+            $this->owner instanceof DevBuildController ||
+            $this->owner instanceof DatabaseAdmin;
+
+        $isAjax              = Director::is_ajax();
+        $configTable         = "SiteConfig";
+        $dbSchema            = DB::get_schema();
 
         if ($dbSchema->hasTable($configTable)) {
             $configFileds = $dbSchema->fieldList($configTable);
             $isInstalled  = isset($configFileds["RUMKey"]);
         }
 
-        if ($isInstalled && !$isCMS && !$isDevBuild && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
-            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"),
-                                                                  [
+        if ($isInstalled && !$isIgnoredController && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
+            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"), [
                 "RUMKey" => SiteConfig::current_site_config()->RUMKey,
             ]);
         }
