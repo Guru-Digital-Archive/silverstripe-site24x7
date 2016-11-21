0efccfe1fbc73ca5d5c1dc18d857c8e4111f49e4
Corey Sewell <corey@gurudigital.nz>
2016-11-21 14:59:51 +1300

    Dont do anything if dev build is being called

--- code/Extension/Site24x7ControllerExtension.php
+++ code/Extension/Site24x7ControllerExtension.php
@@ -9,6 +9,7 @@ class Site24x7ControllerExtension extends Extension
     {
         $isInstalled = false;
         $isCMS       = (bool) is_subclass_of($this->owner, "LeftAndMain");
+        $isDevBuild  = (bool) is_subclass_of($this->owner, "DevBuildController");
         $isAjax      = Director::is_ajax();
         $configTable = "SiteConfig";
         $dbSchema    = DB::get_schema();
@@ -18,8 +19,9 @@ class Site24x7ControllerExtension extends Extension
             $isInstalled  = isset($configFileds["RUMKey"]);
         }
 
-        if ($isInstalled && !$isCMS && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
-            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"), [
+        if ($isInstalled && !$isCMS && !$isDevBuild && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
+            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"),
+                                                                  [
                 "RUMKey" => SiteConfig::current_site_config()->RUMKey,
             ]);
         }
