eeb4e6a285311494f6e05d63f9aa1a037b3046c3
Corey Sewell <corey@gurudigital.nz>
2016-11-21 14:09:30 +1300

    Check db has been built before including JS

--- code/Extension/Site24x7ControllerExtension.php
+++ code/Extension/Site24x7ControllerExtension.php
@@ -7,10 +7,20 @@ class Site24x7ControllerExtension extends Extension
 {
     public function onBeforeInit()
     {
-        $isCMS  = (bool) is_subclass_of($this->owner, "LeftAndMain");
-        $isAjax = Director::is_ajax();
-        if (!$isCMS && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
-            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"), [
+        $isCMS       = (bool) is_subclass_of($this->owner, "LeftAndMain");
+        $isAjax      = Director::is_ajax();
+        $configTable = SiteConfig::current_site_config()->baseTable();
+        $dbSchema    = DB::get_schema();
+
+        $isInstalled = false;
+        if ($dbSchema->hasTable($configTable)) {
+            $configFileds = $dbSchema->fieldList($configTable);
+            $isInstalled  = isset($configFileds["RUMKey"]);
+        }
+
+        if ($isInstalled && !$isCMS && !$isAjax && !empty(SiteConfig::current_site_config()->RUMKey)) {
+            Requirements::javascriptTemplate(Director::getAbsFile(SS_SITE_24X7_DIR."/assets/javascript/site24x7.template.js"),
+                                                                  [
                 "RUMKey" => SiteConfig::current_site_config()->RUMKey,
             ]);
         }
